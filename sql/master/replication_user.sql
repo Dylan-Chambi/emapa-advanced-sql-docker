361dcf0492198ffeeaa228450c6d0539832d9603
Dylan-Chambi <dylanchambifi@gmail.com>
2023-06-11 19:38:00 -0400

    Updating scheme

--- sql/master/replication_user.sql
+++
@@ -1,2 +0,0 @@
-CREATE USER 'replication_user'@'%' IDENTIFIED BY 'replication123';
-GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%';
--- sql/scheme.sql
+++ sql/scheme.sql
@@ -14,19 +14,42 @@ CREATE TABLE IF NOT EXISTS empresa (
     PRIMARY KEY (id_empresa)
 );
 
+CREATE TABLE IF NOT EXISTS direccion (
+    id_direccion INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    calle_o_avenida_direccion VARCHAR(255) NOT NULL,
+    nro_domicilio_direccion INT UNSIGNED NOT NULL,
+    zona_direccion VARCHAR(50) NOT NULL,
+    departamento_direccion VARCHAR(50) NOT NULL
+    PRIMARY KEY (id_direccion)
+);
+
+CREATE TABLE IF NOT EXISTS sucursal(
+    id_sucursal INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    direccion_sucursal_id INT UNSIGNED NOT NULL,
+    nombre_sucursal VARCHAR(100) NOT NULL,
+    telefono_sucursal VARCHAR(20) NOT NULL,
+    correo_sucursal VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id_sucursal),
+    FOREIGN KEY (direccion_sucursal_id) REFERENCES direccion (id_direccion)
+);
+
 CREATE TABLE IF NOT EXISTS almacen (
     id_almacen INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     nombre_almacen VARCHAR(100) NOT NULL,
-    lugar_almacen VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_almacen)
+    nro_piso_almacen INT UNSIGNED NOT NULL,
+    nro_cuarto_almacen INT UNSIGNED NOT NULL,
+    sucursal_almacen INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_almacen),
+    FOREIGN KEY (sucursal_almacen) REFERENCES sucursal (id_sucursal)
 );
 
 CREATE TABLE IF NOT EXISTS cliente (
     nit_ci BIGINT UNSIGNED NOT NULL UNIQUE,
-    nombres_cliente VARCHAR(50) NOT NULL,
+    nombre_cliente VARCHAR(50) NOT NULL,
+    segundo_nombre VARCHAR(50),
     apellido_paterno VARCHAR(50) NOT NULL,
     apellido_materno VARCHAR(50) NOT NULL,
-    corre_cliente VARCHAR(30) NOT NULL,
+    correo_cliente VARCHAR(30) NOT NULL,
     telefono_cliente VARCHAR(20) NOT NULL,
     extension_ci VARCHAR(5) NOT NULL,
     PRIMARY KEY (nit_ci)
@@ -46,8 +69,10 @@ CREATE TABLE IF NOT EXISTS proveedor (
     empresa_id INT UNSIGNED NOT NULL,
     telefono_proveedor VARCHAR(20),
     encargado_proveedor VARCHAR(50),
+    direccion_proveedor_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id_proveedor),
-    FOREIGN KEY (empresa_id) REFERENCES empresa (id_empresa)
+    FOREIGN KEY (empresa_id) REFERENCES empresa (id_empresa),
+    FOREIGN KEY (direccion_proveedor_id) REFERENCES direccion (id_direccion)
 );
 
 CREATE TABLE IF NOT EXISTS factura (
@@ -69,20 +94,27 @@ CREATE TABLE IF NOT EXISTS factura (
 ) AUTO_INCREMENT = 10000000;
 
 CREATE TABLE IF NOT EXISTS producto (
-    id_producto INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    id_producto BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     nombre_producto VARCHAR(100) NOT NULL,
     proveedor_id INT UNSIGNED NOT NULL,
     codigo_barras BIGINT UNSIGNED,
     precio_nacional DECIMAL(10, 2) NOT NULL,
     marca_id INT UNSIGNED NOT NULL,
     categoria_id INT UNSIGNED NOT NULL,
-    fecha_vencimiento DATE NOT NULL,
     estado_producto ENUM('HABILITADO', 'DESHABILITADO'),
+    cantidad_stock_total INT UNSIGNED NOT NULL,
+    cantidad_vendida_total INT UNSIGNED NOT NULL,
     PRIMARY KEY (id_producto),
     FOREIGN KEY (proveedor_id) REFERENCES proveedor (id_proveedor),
     FOREIGN KEY (marca_id) REFERENCES marca (id_marca),
     FOREIGN KEY (categoria_id) REFERENCES categoria (id_categoria)
 );
+
+CREATE TABLE IF NOT EXISTS almacen_producto (
+    producto_id BIGINT UNSIGNED NOT NULL,
+    almacen_id INT UNSIGNED NOT NULL,
+    cantidad_producto INT UNSIGNED NOT NULL,
+)
 
 CREATE TABLE IF NOT EXISTS compra (
     id_compra INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
@@ -93,15 +125,13 @@ CREATE TABLE IF NOT EXISTS compra (
     FOREIGN KEY (proveedor_id) REFERENCES proveedor (id_proveedor)
 );
 
-CREATE TABLE IF NOT EXISTS detalle_compra (
-    id_detalle_compra INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+CREATE TABLE IF NOT EXISTS lote_compra (
     compra_id INT UNSIGNED NOT NULL,
     producto_id INT UNSIGNED NOT NULL,
-    cantidad_producto MEDIUMINT NOT NULL,
-    CHECK (cantidad_producto > 0),
+    fecha_vencimiento_lote DATE NOT NULL,
+    cantidad_producto MEDIUMINT UNSIGNED NOT NULL,
     precio_unitario DECIMAL(10, 2) NOT NULL,
-    total_detalle_compra DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (id_detalle_compra),
+    total_lote_compra DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (compra_id) REFERENCES compra (id_compra),
     FOREIGN KEY (producto_id) REFERENCES producto (id_producto)
 );
@@ -110,23 +140,20 @@ CREATE TABLE IF NOT EXISTS venta (
     id_venta INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     fecha_venta DATE NOT NULL,
     cliente_nit_ci BIGINT UNSIGNED NOT NULL,
-    estado_venta ENUM('INMEDIATA', 'PENDIENTE', 'PAGADA'),
-    factura_nro BIGINT UNSIGNED NOT NULL,
-    total_venta DECIMAL(10, 2),
+    estado_venta ENUM('INMEDIATA', 'PENDIENTE', 'PAGADA') NOT NULL,
+    factura_nro BIGINT UNSIGNED NOT NULL UNIQUE,
+    total_venta DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (id_venta),
     FOREIGN KEY (cliente_nit_ci) references cliente (nit_ci),
     FOREIGN KEY (factura_nro) references factura (nro_factura)
 );
 
-CREATE TABLE IF NOT EXISTS detalle_venta (
-    id_detalle_venta INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+CREATE TABLE IF NOT EXISTS lote_venta (
     venta_id INT UNSIGNED NOT NULL,
     producto_id INT UNSIGNED NOT NULL,
-    cantidad_producto MEDIUMINT NOT NULL,
-    CHECK (cantidad_producto > 0),
+    cantidad_producto MEDIUMINT UNSIGNED NOT NULL,
     precio_unitario DECIMAL(10, 2) NOT NULL,
-    total_detalle_compra DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (id_detalle_venta),
+    total_lote_compra DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (venta_id) REFERENCES venta (id_venta),
     FOREIGN KEY (producto_id) REFERENCES producto (id_producto)
 );
@@ -137,6 +164,7 @@ CREATE TABLE IF NOT EXISTS movimiento_producto (
     almacen_origen_id INT UNSIGNED NOT NULL,
     almacen_destino_id INT UNSIGNED NOT NULL,
     fecha_movimiento DATE NOT NULL,
+    cantidad_movimiento INT UNSIGNED NOT NULL,
     PRIMARY KEY (id_movimiento),
     FOREIGN KEY (producto_id) REFERENCES producto (id_producto),
     FOREIGN KEY (almacen_origen_id) REFERENCES almacen (id_almacen),
